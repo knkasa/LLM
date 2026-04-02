bc05fa7ed9e588ad696f483e88cfacf339fb4911
Ken Nakatsukasa <knkasa@gmail.com>
2026-04-02 16:34:31 +0900

    Update langgraph_example2.py

--- langgraph_example2.py
+++ langgraph_example2.py
@@ -1,5 +1,6 @@
 # https://kadermiyanyedi.medium.com/building-an-ocr-react-agent-from-scratch-with-langgraph-and-gemini-8884b4fbde00?source=email-00a0275a2be7-1773165050524-digest.reader--8884b4fbde00----8-98------------------688df3cd_e0fc_4e67_86a7_f362d39e45ec-1
 # combine this with crewAI etc.
+# Also, see LLM/owlready2_example.py
 
 from typing import TypedDict
 from langgraph.graph import StateGraph, END, START
